5d54c7e9d23f75032121cf5aaa8b27e8914a5c10
Evan Lin <evanslin@gmail.com>
2018-06-05 02:46:57 +0800

    Change to JSON for object and rename original one to string in Queue

--- queue.go
+++ queue.go
@@ -1,6 +1,11 @@
 package redis
 
-import redigo "github.com/gomodule/redigo/redis"
+import (
+	"encoding/json"
+	"fmt"
+
+	redigo "github.com/gomodule/redigo/redis"
+)
 
 // Queue: Task queue to process with key
 type Queue struct {
@@ -14,14 +19,38 @@ func NewQueue(conn *Connection) *Queue {
 	}
 }
 
-// Enqueue with key
-func (q *Queue) Enqueue(key string, value interface{}) (n int, err error) {
+// EnqueueString with key
+func (q *Queue) EnqueueString(key string, value string) (n int, err error) {
 	return redigo.Int(q.Do("RPUSH", key, value))
 }
 
-// Dequeue with key
-func (q *Queue) Dequeue(key string) (string, error) {
+// EnqueueJSON with key, value could be interface object with JSON
+func (q *Queue) EnqueueJSON(key string, value interface{}) (n int, err error) {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		return 0, err
+	}
+
+	return redigo.Int(q.Do("RPUSH", key, bytes))
+}
+
+// DequeueString with key
+func (q *Queue) DequeueString(key string) (string, error) {
 	return redigo.String(q.Do("LPOP", key))
+}
+
+// Dequeue with key
+func (q *Queue) DequeueJSON(key string) ([]byte, error) {
+	val, err := q.Do("LPOP", key)
+	if err != nil {
+		return nil, err
+	}
+	switch v := val.(type) {
+	case []byte:
+		return v, nil
+	}
+
+	return nil, fmt.Errorf("%s", "Error in type assertion")
 }
 
 // Cleanup all data in specific key in queue
--- queue_test.go
+++ queue_test.go
@@ -1,12 +1,18 @@
 package redis
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
+type TestTask struct {
+	V1 int     `json:"v1"`
+	V2 string  `json:"v2"`
+	V3 *string `json:"v3"`
+}
 type QueueTestSuite struct {
 	suite.Suite
 	service *Service
@@ -31,21 +37,47 @@ func (suite *QueueTestSuite) TestSingleQueue() {
 	_, err := q.RemoveAll("test1")
 	assert.NoError(suite.T(), err)
 
-	n, err := q.Enqueue("test1", "value1")
+	n, err := q.EnqueueString("test1", "value1")
 	assert.NoError(suite.T(), err)
 	assert.NotZero(suite.T(), n)
 
-	n2, err2 := q.Enqueue("test1", "value2")
+	n2, err2 := q.EnqueueString("test1", "value2")
 	assert.NoError(suite.T(), err2)
 	assert.NotZero(suite.T(), n2)
 
-	v1, err3 := q.Dequeue("test1")
+	v1, err3 := q.DequeueString("test1")
 	assert.NoError(suite.T(), err3)
 	assert.Equal(suite.T(), "value1", v1)
 
-	v2, err4 := q.Dequeue("test1")
+	v2, err4 := q.DequeueString("test1")
 	assert.NoError(suite.T(), err4)
 	assert.Equal(suite.T(), "value2", v2)
+}
+
+func (suite *QueueTestSuite) TestSingleQueueInterface() {
+	q := NewQueue(suite.conn)
+	assert.NotNil(suite.T(), q)
+
+	v3 := "v3"
+	task := &TestTask{
+		V1: 5,
+		V2: "v2",
+		V3: &v3,
+	}
+
+	_, err := q.RemoveAll("test3")
+	assert.NoError(suite.T(), err)
+
+	n, err := q.EnqueueJSON("test3", task)
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
+
+	byt, err3 := q.DequeueJSON("test3")
+	v := &TestTask{}
+	err = json.Unmarshal(byt, v)
+	assert.NoError(suite.T(), err3)
+	assert.Equal(suite.T(), "v2", v.V2)
+	assert.Equal(suite.T(), 5, v.V1)
 }
 
 func (suite *QueueTestSuite) TestMultipleQueue() {
@@ -58,35 +90,35 @@ func (suite *QueueTestSuite) TestMultipleQueue() {
 	_, err = q.RemoveAll("test2")
 	assert.NoError(suite.T(), err)
 
-	n, err := q.Enqueue("test1", "value1")
+	n, err := q.EnqueueString("test1", "value1")
 	assert.NoError(suite.T(), err)
 	assert.NotZero(suite.T(), n)
 
-	n, err = q.Enqueue("test2", "value3")
+	n, err = q.EnqueueString("test2", "value3")
 	assert.NoError(suite.T(), err)
 	assert.NotZero(suite.T(), n)
 
-	n, err = q.Enqueue("test1", "value2")
+	n, err = q.EnqueueString("test1", "value2")
 	assert.NoError(suite.T(), err)
 	assert.NotZero(suite.T(), n)
 
-	n, err = q.Enqueue("test2", "value4")
+	n, err = q.EnqueueString("test2", "value4")
 	assert.NoError(suite.T(), err)
 	assert.NotZero(suite.T(), n)
 
-	v1, err := q.Dequeue("test1")
+	v1, err := q.DequeueString("test1")
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), "value1", v1)
 
-	v2, err := q.Dequeue("test1")
+	v2, err := q.DequeueString("test1")
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), "value2", v2)
 
-	v3, err := q.Dequeue("test2")
+	v3, err := q.DequeueString("test2")
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), "value3", v3)
 
-	v4, err := q.Dequeue("test2")
+	v4, err := q.DequeueString("test2")
 	assert.NoError(suite.T(), err)
 	assert.Equal(suite.T(), "value4", v4)
 }
