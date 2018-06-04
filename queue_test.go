4044d8ed020dd31314df78e63e47362bf3f96b72
Evan Lin <evanslin@gmail.com>
2018-06-04 14:44:41 +0800

    Change to test suite

--- queue_test.go
+++ queue_test.go
@@ -4,93 +4,93 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestSingleQueue(t *testing.T) {
-	redisConfig := &RedisConfig{
-		Host: "localhost",
-		Port: 6379,
-	}
-
-	redisService := New(redisConfig)
-
-	conn := redisService.GetConnection()
-	defer conn.Close()
-
-	q := NewQueue(conn)
-	assert.NotNil(t, q)
-
-	_, err := q.RemoveAll("test1")
-	assert.NoError(t, err)
-
-	n, err := q.Enqueue("test1", "value1")
-	assert.NoError(t, err)
-	t.Log(n)
-	assert.NotZero(t, n)
-
-	n2, err2 := q.Enqueue("test1", "value2")
-	assert.NoError(t, err2)
-	t.Log(n2)
-	assert.NotZero(t, n2)
-
-	v1, err3 := q.Dequeue("test1")
-	assert.NoError(t, err3)
-	assert.Equal(t, "value1", v1)
-
-	v2, err4 := q.Dequeue("test1")
-	assert.NoError(t, err4)
-	assert.Equal(t, "value2", v2)
+type QueueTestSuite struct {
+	suite.Suite
+	service *Service
+	conn    *Connection
 }
 
-func TestMultipleQueue(t *testing.T) {
-	redisConfig := &RedisConfig{
-		Host: "localhost",
-		Port: 6379,
-	}
+func (suite *QueueTestSuite) SetupTest() {
+	suite.service = NewTestRedisService()
+	assert.NotNil(suite.T(), suite.service)
+	suite.conn = suite.service.GetConnection()
+	assert.NotNil(suite.T(), suite.conn)
+}
 
-	redisService := New(redisConfig)
+func (suite *QueueTestSuite) TearDownTest() {
+	defer suite.conn.Close()
+}
 
-	conn := redisService.GetConnection()
-	defer conn.Close()
-
-	q := NewQueue(conn)
-	assert.NotNil(t, q)
+func (suite *QueueTestSuite) TestSingleQueue() {
+	q := NewQueue(suite.conn)
+	assert.NotNil(suite.T(), q)
 
 	_, err := q.RemoveAll("test1")
-	assert.NoError(t, err)
-
-	_, err = q.RemoveAll("test2")
-	assert.NoError(t, err)
+	assert.NoError(suite.T(), err)
 
 	n, err := q.Enqueue("test1", "value1")
-	assert.NoError(t, err)
-	assert.NotZero(t, n)
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
+
+	n2, err2 := q.Enqueue("test1", "value2")
+	assert.NoError(suite.T(), err2)
+	assert.NotZero(suite.T(), n2)
+
+	v1, err3 := q.Dequeue("test1")
+	assert.NoError(suite.T(), err3)
+	assert.Equal(suite.T(), "value1", v1)
+
+	v2, err4 := q.Dequeue("test1")
+	assert.NoError(suite.T(), err4)
+	assert.Equal(suite.T(), "value2", v2)
+}
+
+func (suite *QueueTestSuite) TestMultipleQueue() {
+	q := NewQueue(suite.conn)
+	assert.NotNil(suite.T(), q)
+
+	_, err := q.RemoveAll("test1")
+	assert.NoError(suite.T(), err)
+
+	_, err = q.RemoveAll("test2")
+	assert.NoError(suite.T(), err)
+
+	n, err := q.Enqueue("test1", "value1")
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
 
 	n, err = q.Enqueue("test2", "value3")
-	assert.NoError(t, err)
-	assert.NotZero(t, n)
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
 
 	n, err = q.Enqueue("test1", "value2")
-	assert.NoError(t, err)
-	assert.NotZero(t, n)
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
 
 	n, err = q.Enqueue("test2", "value4")
-	assert.NoError(t, err)
-	assert.NotZero(t, n)
+	assert.NoError(suite.T(), err)
+	assert.NotZero(suite.T(), n)
 
 	v1, err := q.Dequeue("test1")
-	assert.NoError(t, err)
-	assert.Equal(t, "value1", v1)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), "value1", v1)
 
 	v2, err := q.Dequeue("test1")
-	assert.NoError(t, err)
-	assert.Equal(t, "value2", v2)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), "value2", v2)
 
 	v3, err := q.Dequeue("test2")
-	assert.NoError(t, err)
-	assert.Equal(t, "value3", v3)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), "value3", v3)
 
 	v4, err := q.Dequeue("test2")
-	assert.NoError(t, err)
-	assert.Equal(t, "value4", v4)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), "value4", v4)
+}
+
+func TestQueueTestSuite(t *testing.T) {
+	suite.Run(t, new(QueueTestSuite))
 }
